91d3f3ad553c120e47686ef57f563997e2d5c75f
Adefolarin Adeniji <adefolarin.adeniji@gmail.com>
2022-04-30 17:34:23 +0100

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ec2_html.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
